8491a79ddd422b7572641a5fc9bb28ec65ac197b
freddiek4 <114713210+freddiek4@users.noreply.github.com>
2024-11-28 03:19:54 -0800

    Update web.py

--- web_scrapping_data /web.py
+++ web_scrapping_data /web.py
@@ -1,1 +1,11 @@
+import requests
+import lxml.html as lx
+import re
+import time
+import nltk
+import numpy as np
 
+import matplotlib.pyplot as plt # or your favorite alternative
+
+from sklearn.preprocessing import normalize
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
